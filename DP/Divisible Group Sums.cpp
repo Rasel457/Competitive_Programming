34407933c68c83f0717ab1c54b10cccce0243c5a
Rasel457 <rasel.aiub990a2gmail.com>
2023-03-09 03:13:13 +0600

    Divisible Group Sums

---
+++ DP/Divisible Group Sums.cpp
@@ -0,0 +1,119 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define vi vector<int>
+#define vl vector<ll>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vii vector<pii>
+#define vll vector<pll>
+#define ff first
+#define ss second
+#define pb push_back
+#define PI 2.0 * acos(0.0)
+#define mem(a, b) memset(a, b, sizeof(a))
+#define forstl(i, s)  for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)// Delete Specific Element from dequeue
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+/*using namespace __gnu_pbds;
+///   *Set.find_by_order(k)  returns a pointer to the k'th element (0 indexed)
+///   Set.order_of_key(k)   returns an integer equal to the number of elements STRICTLY SMALLER than 'k'
+template<typename T> using orderSet = tree<T,null_type,less_equal<T>,rb_tree_tag,tree_order_statistics_node_update>;*/
+
+
+ll gcd(ll a, ll b) {return __gcd(a,b);}
+ll lcm(ll a, ll b) {return a*(b/gcd(a,b));}
+
+int stringTointeger(string str)
+{
+    int temp = 0;
+    for (int i = 0; i < str.length(); i++) {
+        temp = temp * 10 + (str[i] - '0');
+    }
+    return temp;
+}
+
+
+const int N = 2e5+20;
+const int MOD = 1e9+7;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000;
+const ld eps=1e-6 ;// 5 decimal point same
+const int maxn = 1200;
+
+int n,q,D,m;
+ll dp[205][25][12];
+ll a[205];
+
+ll print(int pos,int rem,int cnt)
+{
+    if(pos>n || m==cnt)
+    {
+        if(rem==0 && m==cnt)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    if(dp[pos][rem][cnt]!=-1)
+    {
+        return dp[pos][rem][cnt];
+    }
+    int rem1=(a[pos]+rem)%D;
+    if(rem1<0)rem1+=D;
+
+    ll x=print(pos+1,rem1,cnt+1);
+    ll y=print(pos+1,rem,cnt);
+
+    return dp[pos][rem][cnt]=x+y;
+}
+
+
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    int t, cas=0;
+    cin>>t;
+    while(t--)
+    {
+        cas++;
+        cin>>n>>q;
+        for(int i=1;i<=n;i++)
+        {
+            cin>>a[i];
+        }
+
+        cout<<"Case "<<cas<<":"<<endl;
+
+        for(int i=1;i<=q;i++)
+        {
+            cin>>D>>m;
+            mem(dp,-1);
+            cout<<print(1,0,0)<<endl;
+        }
+
+
+
+    }
+
+
+    return 0;
+
+}
+
+//https://vjudge.net/problem/LightOJ-1125
+//https://vjudge.net/problem/UVA-10616
