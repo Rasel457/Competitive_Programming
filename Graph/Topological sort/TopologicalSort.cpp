77db08845f20df2078b4ffd600745d9e233b7f1c
Rasel457 <rasel.aiub990a2gmail.com>
2023-06-15 20:26:06 +0600

    Topological sort spoj problem

---
+++ Graph/Topological sort/TopologicalSort.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define ff first
+#define ss second
+#define pb push_back
+#define mem(a, b) memset(a, b, sizeof(a))
+ 
+const int N = 1e4+5;
+const int MOD = 1e9+7;
+const int maxn = 1005;
+const int INF=1e8;
+ 
+//U,D,L,R
+char dir[4]={'U','D','L','R'};
+int dx[]={-1, 1, 0, 0, -1, -1, 1, 1};
+int dy[]={0, 0, -1, 1, -1, 1, -1, 1};
+ 
+int kx[]={1, 1, 2, 2, -1, -1, -2, -2};
+int ky[]={2, -2, 1, -1, 2, -2, 1, -1};
+ 
+vector<int>adj[N];
+vector<int>sorted;
+int visited[N];
+bool flag=false;
+ 
+void dfs(int u)
+{
+    if(flag==true)
+    {
+     	return;
+    }
+    visited[u]=1;
+    sort(adj[u].begin(),adj[u].end());
+    for(int i=adj[u].size()-1;i>=0;i--)
+    {
+        int v=adj[u][i];
+        if(visited[v]==0)
+        {
+            dfs(v);
+        }
+        if(visited[v]==1)
+        {
+           flag=true;
+           return;
+        }
+    }
+    visited[u]=2;
+    sorted.push_back(u); // All child explored
+}
+ 
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+ 
+    int nodes,edges;
+    cin>>nodes>>edges;
+ 
+    int x,y;
+    for(int i=1;i<=edges;i++)
+    {
+        cin>>x>>y;
+        adj[x].push_back(y);
+ 
+    }
+ 
+    for(int i=nodes;i>=1;i--)
+    {
+        if(visited[i]==0)
+        {
+            dfs(i);
+        }
+    }
+ 
+    if(flag==true)
+    {
+        cout<<"Sandro fails."<<endl;
+    }
+    else
+    {
+	    reverse(sorted.begin(),sorted.end());
+        for(int i=0;i<sorted.size();i++)
+        {
+            cout<<sorted[i]<<" ";
+        }
+        cout<<endl;
+    }
+ 
+ 
+    return 0;
+}
+
+//https://www.spoj.com/problems/TOPOSORT/
